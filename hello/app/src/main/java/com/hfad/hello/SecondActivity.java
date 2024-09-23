28026ff167847baedaea70807d9ea1304808654a
popochip <phambadatktpm@gmail.com>
2024-09-23 09:09:30 +0700

    them layout cho SecondActivity

--- hello/app/src/main/java/com/hfad/hello/SecondActivity.java
+++ hello/app/src/main/java/com/hfad/hello/SecondActivity.java
@@ -1,7 +1,15 @@
 package com.hfad.hello;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
 }
